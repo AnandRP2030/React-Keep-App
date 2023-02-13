d312f655e127d9f66507e9482659f00b1f123418
Anand R P <anand.rp2030@gmail.com>
2023-02-13 23:41:16 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# React-Keep App
+## React keep app is a clone of Google keep app
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App (){
+  return (
+    <div> 
+      <h1> Google Keep </h1>
+    </div>
+  )
+}
+
+export default App;
